278bef479409b64e196709eb7800550c97f4d471
afernv10 <afernv10@estudiantes.unileon.es>
2019-03-03 13:43:32 +0100

    source format

--- Practica01/src/Practica.java
+++ Practica01/src/Practica.java
@@ -4,118 +4,98 @@ import java.util.Comparator;
 
 public class Practica {
 
-	private static  String texto = "La noche cae, brumosa ya y morada. "
-					+ "Vagas claridades malvas y verdes perduran tras la torre de la iglesia. "
-					+ "El camino sube, lleno de sombras, de campanillas, de fragancia de hierba, "
-					+ "de canciones, de cansancio y de anhelo.";
-	
+	private static String texto = "La noche cae, brumosa ya y morada. "
+			+ "Vagas claridades malvas y verdes perduran tras la torre de la iglesia. "
+			+ "El camino sube, lleno de sombras, de campanillas, de fragancia de hierba, "
+			+ "de canciones, de cansancio y de anhelo.";
+
 	private static ArrayList<Letra> letras = new ArrayList<Letra>();
-	
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		llenar();
 		calcularProbabilidad();
-		//ordenar();
+		// ordenar();
 		ArrayList<Letra> ordenada = new ArrayList<Letra>();
-		
-		
+
 		imprimir();
-		Collections.sort(letras,new ComparadorNombre());
+		Collections.sort(letras, new ComparadorNombre());
 		System.out.println();
 		imprimir();
-		
-	}
-	
-	
-	 
-	private static void ordenar() {
-		
-		ArrayList<Letra> ordenada = new ArrayList<Letra>();
-		
-		for(Letra l : letras) {
-			
-			
-		}
-	}
 
-
+	}
 
 	private static void calcularProbabilidad() {
-		
+
 		int suma_total = 0;
-		
-		for(Letra l : letras) {
-			
+
+		for (Letra l : letras) {
+
 			suma_total += l.getFrecuencia();
 		}
 		System.out.println(suma_total);
-		
-		for(int i = 0; i<letras.size(); i++) {
-			
-			double probabilidad = (double)letras.get(i).getFrecuencia()/suma_total;
-			
-			
+
+		for (int i = 0; i < letras.size(); i++) {
+
+			double probabilidad = (double) letras.get(i).getFrecuencia() / suma_total;
+
 			letras.get(i).setProbabilidad(probabilidad);
-			
+
 		}
 	}
 
+	public static void llenar() {
 
+		for (int i = 0; i < texto.length(); i++) {
 
-	public static void llenar(){
-		
-		for(int i = 0; i< texto.length(); i++) {
-			
-			 char c = texto.charAt(i);
-			 
-			 if(letras.isEmpty()) {
-				 
-				 Letra nueva = new Letra(c);
-				 letras.add(nueva);
-			 }
-			 else if(esNuevo(c)) {
-				
-				 letras.add(new Letra(c));
-				
-			 }		
-		}	
+			char c = texto.charAt(i);
+
+			if (letras.isEmpty()) {
+
+				Letra nueva = new Letra(c);
+				letras.add(nueva);
+			} else if (esNuevo(c)) {
+
+				letras.add(new Letra(c));
+
+			}
+		}
 	}
-	
+
 	public static boolean esNuevo(char letra) {
-		
-		
-		for(Letra l : letras) {
-			//System.out.println("l["+l.getLetra()+"] letra["+letra+"]");
-			
-			if(l.getLetra() == letra) {
+
+		for (Letra l : letras) {
+			// System.out.println("l["+l.getLetra()+"] letra["+letra+"]");
+
+			if (l.getLetra() == letra) {
 				l.setFrecuencia(1);
 				return false;
 			}
 		}
 		return true;
 	}
-	public static  void imprimir() {
-		for(Letra l : letras) {
-			 if(l.getLetra() == ' ') {
-				 l.setLetra('-');
-			 }
-			System.out.println(l.toString()+"\tFrecuencia["+l.getFrecuencia()+"]\tProbabilidad["+String.format("%.3f",l.getProbabilidad())+"]");
+
+	public static void imprimir() {
+		for (Letra l : letras) {
+			if (l.getLetra() == ' ') {
+				l.setLetra('-');
+			}
+			System.out.println(l.toString() + "\tFrecuencia[" + l.getFrecuencia() + "]\tProbabilidad["
+					+ String.format("%.3f", l.getProbabilidad()) + "]");
 		}
 	}
 
 }
 
-
 class ComparadorNombre implements Comparator<Letra> {
- 
-    @Override
-    public int compare(Letra l1, Letra l2) {
-        // TODO Auto-generated method stub
-    	if (l1.getProbabilidad() < l2.getProbabilidad()) return 1;
-        if (l1.getProbabilidad() > l2.getProbabilidad()) return -1;
-        return 0;
-    }
- 
+
+	@Override
+	public int compare(Letra l1, Letra l2) {
+		if (l1.getProbabilidad() < l2.getProbabilidad())
+			return 1;
+		if (l1.getProbabilidad() > l2.getProbabilidad())
+			return -1;
+		return 0;
+	}
+
 }
