657eff34cba46eaf6d0859a7d7a9cf682093d092
afernv10 <afernv10@estudiantes.unileon.es>
2019-03-03 13:53:42 +0100

    TODO p2

---
+++ Practica02/src/HuffmanBinario.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class HuffmanBinario {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] probs = {0.3, 0.2, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05};
+		ArrayList<Numero> numeros = new ArrayList<Numero>();
+		int i = 0;
+		//for()
+	}
+
+}
