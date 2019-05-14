026614d1d84bef515995e243625a2381f2e53c61
dmarcs00 <dmarcs00@estudiantes.unileon.es>
2019-05-14 10:45:54 +0200

    RSA Bloques con codificacion

--- Practica09/RSABloques.java
+++ Practica09/RSABloques.java
@@ -31,7 +31,7 @@ public class RSABloques {
 		e=80263681
 		n=27264083009 * 27264083017
 
-		MarÃ­a
+		MarÃƒÂ­a
 		n=8849169404252643679
 		e=196413997
 		n=2974755337 * 2974755367
@@ -42,12 +42,12 @@ public class RSABloques {
 		n=2290182972661 * 2290182972709 p y q
 		*/
 		//System.out.println(alf.length());
-		//String modelo1 = "wVBÃ±Ãº94wAU9gaÃ“c66:YCÃºVIwAlk)U9ULBMQ)-7caÃ³NS8nvB08h 8ÃºÃ�tÃ‘J)Â¿sYqLBÃ�4duCsfkÃ³x)aKE9(3:Hf(Â¿NmoGÃ±!DABBÃ‘6eÃ‘rGUPÃ¡bCÃ±tdawqbÃ­VWPbÃ©ecJÃ‘M)LAcÂ¿2ywRrÃ±rth,896u6on?7b5J81v(LFTÃ‰Ã³N?sNB!Ã±r,:b877da4Ã±4??8hdG ";
+		//String modelo1 = "wVBÃƒÂ±ÃƒÂº94wAU9gaÃƒâ€œc66:YCÃƒÂºVIwAlk)U9ULBMQ)-7caÃƒÂ³NS8nvB08h 8ÃƒÂºÃƒï¿½tÃƒâ€˜J)Ã‚Â¿sYqLBÃƒï¿½4duCsfkÃƒÂ³x)aKE9(3:Hf(Ã‚Â¿NmoGÃƒÂ±!DABBÃƒâ€˜6eÃƒâ€˜rGUPÃƒÂ¡bCÃƒÂ±tdawqbÃƒÂ­VWPbÃƒÂ©ecJÃƒâ€˜M)LAcÃ‚Â¿2ywRrÃƒÂ±rth,896u6on?7b5J81v(LFTÃƒâ€°ÃƒÂ³N?sNB!ÃƒÂ±r,:b877da4ÃƒÂ±4??8hdG ";
 
-		//String modelo2 = "ihJACM?NM?IXCÃ‘ÃšÃ­2ZoAÃ³xÂ¿eyK1(Ã­iv7ctw!q2,whCX-x!5Ã‰qÃ±Â¿aÂ¿3CkO?gDsvNOQZEBÃ�Ã‰tbIÃšGTEa-rMmjÂ¿wwpwhN6sJÃ­XbldcÃ‘zZw)XhS hÃ±7Zr8U11YThAÃ³8Ã“5Ã©jÃ­ÃºÂ¿toyI.ÃšqÃ©tÃ�Ã�MÃ¡Tk3MsMpT?,Ã�ÃšMÃ±DaIU9Â¿FÃšdKMcHB!!4:cIÃ©TtB.Ã¡5-8HcÃ©Ã­9T";
+		//String modelo2 = "ihJACM?NM?IXCÃƒâ€˜ÃƒÅ¡ÃƒÂ­2ZoAÃƒÂ³xÃ‚Â¿eyK1(ÃƒÂ­iv7ctw!q2,whCX-x!5Ãƒâ€°qÃƒÂ±Ã‚Â¿aÃ‚Â¿3CkO?gDsvNOQZEBÃƒï¿½Ãƒâ€°tbIÃƒÅ¡GTEa-rMmjÃ‚Â¿wwpwhN6sJÃƒÂ­XbldcÃƒâ€˜zZw)XhS hÃƒÂ±7Zr8U11YThAÃƒÂ³8Ãƒâ€œ5ÃƒÂ©jÃƒÂ­ÃƒÂºÃ‚Â¿toyI.ÃƒÅ¡qÃƒÂ©tÃƒï¿½Ãƒï¿½MÃƒÂ¡Tk3MsMpT?,Ãƒï¿½ÃƒÅ¡MÃƒÂ±DaIU9Ã‚Â¿FÃƒÅ¡dKMcHB!!4:cIÃƒÂ©TtB.ÃƒÂ¡5-8HcÃƒÂ©ÃƒÂ­9T";
 
 		String modelo3 = "júI,¿ Qb9,a5Ljw5DdgnwwEu áFsTlEp.éCkÍR.xjdj0X¿ñ8thÓxuZ.SgÑmAEIÍPJjóíJ2t:7cPApÉC2ncÉÓ?ñám¿J)trZ4gFú?kZÉAXñd6F¿0cm4hO?Jxc,q 2P,V7tóÑndoÚAadÓPAGLcbWDÑ(1pncYPnNKw:iéóEMívfZPDPa6ÍKU3mXkÓ)Ef1eÉy4GjLdxE";
-		//String modelo4 ="BUL mzaR3Qx-OÃ­QDM6EagkWFbbÂ¿V7loJHpÃ“Ã©caxLLBuÃº28VÂ¿Ã�Ã�ETÃ¡Ã�KqEcFÃ³Ã‘UV1vu5rRxIchMiSikUpyAqXuÃ�vjWccDbUV3-S9VMÃšDÃ³Ã©Ã‘KKHdeWfphL2AoHKÃ­BpcuOKO-kÃºYhÃºMNo95uc20sw1BeMÃ±j(v-Ã±z:Ã³MwÃ¡PVjqK66Vonegcy1zvUgAÃ�5eM5YSQG3e)EÃºrmkj";
+		//String modelo4 ="BUL mzaR3Qx-OÃƒÂ­QDM6EagkWFbbÃ‚Â¿V7loJHpÃƒâ€œÃƒÂ©caxLLBuÃƒÂº28VÃ‚Â¿Ãƒï¿½Ãƒï¿½ETÃƒÂ¡Ãƒï¿½KqEcFÃƒÂ³Ãƒâ€˜UV1vu5rRxIchMiSikUpyAqXuÃƒï¿½vjWccDbUV3-S9VMÃƒÅ¡DÃƒÂ³ÃƒÂ©Ãƒâ€˜KKHdeWfphL2AoHKÃƒÂ­BpcuOKO-kÃƒÂºYhÃƒÂºMNo95uc20sw1BeMÃƒÂ±j(v-ÃƒÂ±z:ÃƒÂ³MwÃƒÂ¡PVjqK66Vonegcy1zvUgAÃƒï¿½5eM5YSQG3e)EÃƒÂºrmkj";
 
 		
 		//para sacar K --> floor(logn/log alf.leng)  MAXIMA caso de juan --> 12
@@ -66,7 +66,7 @@ public class RSABloques {
 		}
 	
 		// Juan
-		BigInteger p = new BigInteger("2290182972661");
+		/*BigInteger p = new BigInteger("2290182972661");
 		BigInteger q = new BigInteger("2290182972709");
 		BigInteger n = new BigInteger("5244938048376303456108649");
 		BigInteger e = new BigInteger("114340249");
@@ -128,13 +128,75 @@ public class RSABloques {
 		for (BigInteger bi : modulos) { // tenemos todas las "m", ahora hay que sacar el cociente y el resto de cada una dividiendo entre alf.lengh
 			mensaje_claro += decodificacionNumero(bi);
 		}
-		
+		System.out.println("\n");
 		System.out.println(mensaje_claro);
+		
+		*/
+		
+		/*
+		 * 
+		 * 
+		 *    CODIFICAR
+		 * 
+		 * 
+		 * 
+		 */
+		String mensaje = "Esto es una prueba";
+		
+		BigInteger p = new BigInteger("2974755337");
+		BigInteger q = new BigInteger("2974755367");
+		BigInteger n = new BigInteger("8849169404252643679");
+		BigInteger e = new BigInteger("196413997");
+		BigInteger k = BigInteger.valueOf(9);
+		//BigInteger fi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+		//BigInteger d = e.modInverse(fi);
+		
+		
+		
+		System.out.println("Codificar: "+mensaje);
+		//codificar(mensaje, k.intValue(), map_posiciones);
+		
+		String[] secuencias = getSecuencias(k.intValue(), mensaje);
+		for (int i = 0; i < secuencias.length; i++) {
+			System.out.println(secuencias[i]);
+		}
+		
+		int[][] posiciones = getPosicionesSecuencias(secuencias, k.intValue(), map_posiciones);
+		
+		for (int i = 0; i < posiciones.length; i++) {
+			for (int j = 0; j < posiciones[0].length; j++) {
+				System.out.print(posiciones[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		ArrayList<BigInteger> m = new ArrayList<BigInteger>();
+		//con esto sacamos las "m"
+		for (int i = 0; i < posiciones.length; i++) {
+			m.add(listaNumero(posiciones[i]));
+		}
+		
+		ArrayList<BigInteger> modulos = new ArrayList<BigInteger>();
+		
+		//calculanmos "c"
+		for (BigInteger bi : m) {
+			modulos.add(bi.modPow(e, n)); //aqui dentro se esta calculando todas las "m"
+			
+		}
+		String mensaje_codificado = "";
+		//las c en modulo alf.lenght
+		for (BigInteger bigInteger : modulos) {
+			mensaje_codificado += codificacionNumero(bigInteger, k.intValue());
+		}
+		
+		System.out.println(mensaje_codificado);
+		
 	}
 	
 	
-	
-	public String decodificacionNumero(BigInteger numero) {
+	public String codificacionNumero(BigInteger numero, int k) {
+		
+		
 		String text = "";
 		String mensaje_claro = "";
 		BigInteger resto = BigInteger.valueOf(0);
@@ -148,8 +210,53 @@ public class RSABloques {
 			restos.add(resto);
 		}
 		restos.add(numero);
-
+		
+		
+		
+		System.out.println(restos.toString() +" l: "+restos.size());
+		if(restos.size() == (k-1)) { // Poner k-1
+			restos.add(BigInteger.ZERO);
+		}
 		for (BigInteger bi : restos) {
+			//System.out.println(alf.charAt(bi.intValue()));
+			text = text + alf.charAt(bi.intValue());
+		}
+		
+		for (int i = text.length() - 1; i >= 0; i--) {
+			mensaje_claro = mensaje_claro + text.charAt(i);
+		}
+
+
+		return mensaje_claro;
+	}
+	
+	
+	
+	public String decodificacionNumero(BigInteger numero) {
+		
+		
+		String text = "";
+		String mensaje_claro = "";
+		BigInteger resto = BigInteger.valueOf(0);
+		
+		ArrayList<BigInteger> restos = new ArrayList<BigInteger>();
+		
+		
+		while (numero.divide(BigInteger.valueOf(alf.length())) != BigInteger.valueOf(0)) {
+			resto = numero.remainder(BigInteger.valueOf(alf.length()));
+			numero = numero.divide(BigInteger.valueOf(alf.length()));
+			restos.add(resto);
+		}
+		restos.add(numero);
+		
+		
+		
+		System.out.println(restos.toString() +" l: "+restos.size());
+		if(restos.size() == 11) { // Poner k-1
+			restos.add(BigInteger.ZERO);
+		}
+		for (BigInteger bi : restos) {
+			//System.out.println(alf.charAt(bi.intValue()));
 			text = text + alf.charAt(bi.intValue());
 		}
 		
@@ -224,7 +331,7 @@ public class RSABloques {
 		int modelo_long = modelo.length();
 		
 		
-		String[] secuencias = new String[modelo_long/grupos]; //TODO comprobar si la division no es exacta para aÃ±adir una posicion mas al vector
+		String[] secuencias = new String[modelo_long/grupos]; //TODO comprobar si la division no es exacta para aÃƒÂ±adir una posicion mas al vector
 		
 		int bloquesNi = 0, bloquesNj = grupos;
 		for (int i = 0; i < secuencias.length; i++) {
